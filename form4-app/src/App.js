82b4a336c527dd24c459a9d7a9a8a16fb66f64e2
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-18 13:13:07 +0530

    Done

--- form4-app/src/App.js
+++ form4-app/src/App.js
@@ -11,7 +11,8 @@ export class App extends Component {
       l1 : '#e04014',
       l2 : '#f07451',
       l3 : '#ed947b',
-      l4 : '#e0a899'
+      l4 : '#e0a899',
+      txt : "black"
     }
   }
   darkmode = (e)=>{
@@ -19,7 +20,8 @@ export class App extends Component {
       l1 : 'black',
       l2 : '#3b3c3d',
       l3 : '#5f6163',
-      l4 : '#97999c'
+      l4 : '#97999c',
+      txt : "white"
     })
   }
  
@@ -29,17 +31,18 @@ export class App extends Component {
       l1 : '#e04014',
       l2 : '#f07451',
       l3 : '#ed947b',
-      l4 : '#e0a899'
+      l4 : '#e0a899',
+      txt : "black"
     })
   }
   render() {
     return (
       <div className="App">
         
-        <div style={{backgroundColor:this.state.l1}}>Level 1</div>
-        <div style={{backgroundColor:this.state.l2}}>Level 2</div>
-        <div style={{backgroundColor:this.state.l3}}>Level 3</div>
-        <div style={{backgroundColor:this.state.l4}}>Level 4</div>
+        <div style={{backgroundColor:this.state.l1,color:this.state.txt}}>Level 1</div>
+        <div style={{backgroundColor:this.state.l2,color:this.state.txt}}>Level 2</div>
+        <div style={{backgroundColor:this.state.l3,color:this.state.txt}}>Level 3</div>
+        <div style={{backgroundColor:this.state.l4,color:this.state.txt}}>Level 4</div>
         <button onClick={this.darkmode}>Dark Mode</button>
         <button onClick={this.light}>Light Mode</button>
       </div>
